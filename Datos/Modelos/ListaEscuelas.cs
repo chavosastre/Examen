1aa13400e3dd2bf41f335c7296b5b31cffb39d1e
Salvador Sastré <chavosastre@gmail.com>
2019-03-25 17:08:24 -0600

    Add files via upload

---
+++ Datos/Modelos/ListaEscuelas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Modelos
+{
+    public class ListaEscuelas
+    {
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+    }
+}
